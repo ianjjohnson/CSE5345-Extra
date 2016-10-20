aa6f18e950a5bd6c0683d2c0ac1809ff48929668
Ian Johnson <ianjjohnson@icloud.com>
2016-10-20 09:09:14 -0500

    Got state setup on the gameserver side of things

--- lib/extra/game_server.ex
+++ lib/extra/game_server.ex
@@ -5,8 +5,12 @@ defmodule Extra.GameServer do
 
   #Interface
 
-  def start_link(args) do
-    GenServer.start __MODULE__, args, name: @me
+  def start_link(low, high) do
+    GenServer.start __MODULE__, %{low: low, high: high}, name: @me
+  end
+
+  def guess(num) do
+    GenServer.call @me, {:guess, num}
   end
 
 
@@ -14,8 +18,22 @@ defmodule Extra.GameServer do
   #Implementation
 
   def init(args) do
-    {:ok, args}
-    IO.inspect args
+    num = :rand.uniform(args.high-args.low+1) + args.low - 1
+    {:ok, Map.put(args, :num, num)}
   end
+
+  def handle_call {:guess, guess}, _from, state do
+    IO.inspect state
+    cond do
+      guess >  state.num -> IO.puts "high"
+                            {:reply, :high, state}
+      guess <  state.num -> IO.puts "low"
+                            {:reply, :low,  state}
+      guess == state.num -> IO.puts "yes!"
+                            {:reply, :yes,  state}
+    end
+  end
+
+  #IO.puts "I'm thinking of a number between #{Enum.at(args, 0)} and #{Enum.at(args, 1)}."
 
 end
